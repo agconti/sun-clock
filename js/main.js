d35ba8bc313b2424f0079ece693e70ecc0fd4b35
agconti <agc11d@gmail.com>
2015-03-01 22:22:57 -0500

    feat(js/main): upaded horizon scale to max at circle radius
    fix(js/main): added closure back in

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// ;(function(){
+;(function(){
 
 var now = new Date()
   , time = getTime(now)
@@ -54,15 +54,18 @@ function getTime() {
  * Sets the clocks scales to the current svg width and height
  */
 function setScales(){
+  var circleDiameter = svgHeight * 0.35
+    , circleRadius = circleDiameter / 2
+    
   d3.select('svg')
     .attr("height", svgHeight)
     .attr("width", svgWidth)
   radiusScale
-    .range([0, svgHeight * 0.35])
+    .range([0, circleDiameter])
   hourScale
     .rangeBands([-1, svgWidth])
   horizonScale
-    .range([svgHeight, 0, svgHeight])
+    .range([svgHeight, circleRadius, svgHeight])
   legendScale
     .range([legendMargin, svgWidth - legendMargin])
 }
@@ -138,5 +141,5 @@ setInterval(function() {
   tick(time)
 }, frameRate)
 
-// })()
+})()
     
